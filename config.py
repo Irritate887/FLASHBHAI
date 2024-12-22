eadeebab3fe9c184034841a59b22585c64f7ef8a
Irritate887 <S2FLASH@proton.me>
2024-12-22 19:48:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7495499381:AAEwqW5YY6kDuLZFTy3CJOnz0g3AQvkO2Co")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("TG_FLASH92")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7303380062")
+
+
+
+
+
+
+
+  
